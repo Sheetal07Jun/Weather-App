6463aea54d75a07661b2abdfb0ae368351bbab85
Sheetal07Jun <115865296+Sheetal07Jun@users.noreply.github.com>
2024-02-17 13:01:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const apiKey="8a79daf0fa43551c20d4aac1ad0b4f26";
+const apiUrl= "https://api.openweathermap.org/data/2.5/weather?units=metric&q=banglore";
+async function checkWeather()
+{
+    const response= await fetch(apiUrl+``);
+
+}
